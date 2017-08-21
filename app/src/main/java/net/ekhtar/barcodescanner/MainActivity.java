0846cab5a1200a1a491bd8be4bdd2666efd2d31b
Ahmed.a.elfattah <ahmed.a.elfattah88@gmail.com>
2017-08-22 01:29:19 +0200

    some changes

--- app/src/main/java/net/ekhtar/barcodescanner/MainActivity.java
+++ app/src/main/java/net/ekhtar/barcodescanner/MainActivity.java
@@ -1,11 +1,11 @@
 package net.ekhtar.barcodescanner;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -14,13 +14,13 @@ import com.google.zxing.integration.android.IntentResult;
 public class MainActivity extends AppCompatActivity {
 
     Button button;
-    TextView textView;
+    EditText editText;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button=(Button)findViewById(R.id.button);
-        textView=(TextView)findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,10 +30,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
     }
-
     /**
      * event handler for scan button
      * @param view view of the activity
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         integrator.setBeepEnabled(true);
         integrator.setBarcodeImageEnabled(true);
         integrator.setCaptureActivity(AnyOrientationCaptureActivity.class);
+        overridePendingTransition(0, 0);
         integrator.setOrientationLocked(false);
         integrator.initiateScan();
     }
@@ -56,11 +57,9 @@ public class MainActivity extends AppCompatActivity {
         if (scanningResult != null) {
             if(scanningResult.getContents() != null){
                 //we have a result
-                textView.setText(scanningResult.getContents());
-
-            }else{
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                editText.setText(scanningResult.getContents());
             }
+
         }else{
             Toast toast = Toast.makeText(getApplicationContext(),"No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
